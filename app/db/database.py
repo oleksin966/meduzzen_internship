4c33d94ed8c70e6f617fd34430b0d32f29d18553
oleksin966 <39014561+oleksin966@users.noreply.github.com>
2024-04-17 12:21:04 +0300

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from core.config import settings
 from typing import AsyncGenerator
-from fastapi import Depends
 
 DATABASE_URL = settings.database_url
 
